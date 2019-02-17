82ec0cc4c2947ffc51504a3649e99e0a1786036a
Atsushi Mori <ki4070ma@gmail.com>
2019-02-17 12:55:16 +0900

    Used unittest

--- test_selenium.py
+++ test_selenium.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 
 import os
+import unittest
 from selenium import webdriver
 
 from pages.google.search import SearchPage
 
-def test_google():
-    driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'))
-    search_page = SearchPage(driver)
-    search_page.input_keyword('hogehoge')
-    assert search_page.get_input_keyword() == 'hogehoge'
-    import time
-    time.sleep(3)
-    driver.close()
+class GoogleSearchPageTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'))
+
+    def tearDown(self):
+        import time
+        time.sleep(3)  # Just to check the view on browse by me
+        self.driver.close()
+
+    def test_scenario1_search_keyword(self):
+        search_page = SearchPage(self.driver)
+        search_page.input_keyword('hogehoge')
+        assert search_page.get_input_keyword() == 'hogehoge'
+
 
 if __name__ == "__main__":
-    test_google()
+    suite = unittest.TestLoader().loadTestsFromTestCase(GoogleSearchPageTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
