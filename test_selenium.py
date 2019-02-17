1c6c01367e0b84b0e8b5e34393ad9749372bf3ae
Atsushi Mori <ki4070ma@gmail.com>
2019-02-17 11:47:43 +0900

    Removed unnecessary codes

--- test_selenium.py
+++ test_selenium.py
@@ -1,38 +1,7 @@
 #!/usr/bin/python3
 
 import os
-import unittest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import pytest
-import requests
-
-# class GetRoot(unittest.TestCase):
-#     options = Options()
-#     options.add_argument('--headless')
-#     driver = webdriver.Chrome(chrome_options=options)
-#
-#     @classmethod
-#     def setUpClass(cls):
-#         cls.base_url = "https://www.seleniumhq.org/"
-#         cls.driver.implicitly_wait(30)
-#         cls.verificationErrors = []
-#         cls.accept_next_alert = True
-#         cls.driver.get(cls.base_url)
-#
-#     @pytest.mark.parametrize()
-#     def test_get_page(self):
-#         driver = self.driver
-#         r = requests.get(driver.current_url)
-#         self.assertEqual(r.status_code, 200)
-#         title_target = "Selenium - Web Browser Automation"
-#         print(driver.title)
-#         self.assertEqual(title_target, driver.title)
-#
-#     @classmethod
-#     def tearDownClass(cls):
-#         cls.driver.quit()
-
 
 def test_google():
     driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'))
@@ -46,5 +15,4 @@ def test_google():
     driver.close()
 
 if __name__ == "__main__":
-    # unittest.main()
     test_google()
