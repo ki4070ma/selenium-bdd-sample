228c21b1a7a9cf4306dfe1b899171f22c9c56c33
Atsushi Mori <ki4070ma@gmail.com>
2019-02-18 23:33:27 +0900

    Added 1 scenario: User search "Google Testing Blog"

--- features/google.feature
+++ features/google.feature
@@ -1,9 +1,10 @@
 Feature: Google search page
 
-  Scenario: Visit google and check
-     When we visit google
-     Then it should have a title "Google"
-
   Scenario: input word at search page
      When we input "hogehoge" to text box at search page
      Then it should have "hogehoge" at text box
+
+  @test
+  Scenario: search "Google Testing Blog" by google
+     When we search with "Google Testing Blog" as query at search page
+     Then Result page should have "Google Testing Blog" at the top of results
--- features/steps/google.py
+++ features/steps/google.py
@@ -1,18 +1,6 @@
 from behave import *
 
 
-@when('we visit google')
-def step(context):
-    pass
-    # context.browser.get('http://www.google.com')
-
-
-@then('it should have a title "Google"')
-def step(context):
-    pass
-   # assert context.browser.title == "Google"
-
-
 @when('we input "{text}" to text box at search page')
 def step(context, text):
     context.search_page.input_keyword(text)
@@ -21,3 +9,13 @@ def step(context, text):
 @then('it should have "{text}" at text box')
 def step(context, text):
     assert context.search_page.search_query_word == text
+
+
+@when('we search with "{text}" as query at search page')
+def step(context, text):
+    context.search_result_page = context.search_page.search(text)
+
+
+@then('Result page should have "{text}" at the top of results')
+def step(context, text):
+    assert context.search_result_page.result_titles()[0] == text
